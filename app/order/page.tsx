60825024517c31d98781bb2cd035c1bedc1c4187
Jamal Riley <73957695+jamalmriley@users.noreply.github.com>
2026-08-02 17:57:40 -0500

    Update page.tsx

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -2,6 +2,10 @@ export default function OrderPage() {
   return (
     <div>
       <h1 className="h1">Order now</h1>
+      <p>
+        Group ordering: allow each person with link to order like DoorDash AND
+        each person's order to be labeled like Chipotle.
+      </p>
     </div>
   );
 }
